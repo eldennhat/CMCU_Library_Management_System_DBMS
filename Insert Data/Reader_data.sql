fb826df17033f36381c7a1962536e6f5a7978a31
Nhat Minh Quang <eldennhat@gmail.com>
2025-10-22 22:08:47 +0700

    The data of the first 20 readers

---
+++ Insert Data/Reader_data.sql
@@ -0,0 +1,179 @@
+﻿INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10001,
+	N'Đinh Nguyên Hoàng',
+	N'Phường Quan Hoa, Cầu Giấy, Hà Nội',
+	'0873448820'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10002,
+	N'Phạm Tuấn Hưng',
+	N'Phường Cổ Loa, Long Biên, Hà Nội',
+	'0987778822'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10003,
+	N'Nguyễn Thành An',
+	N'Phường Mai Dịch, Cầu Giấy, Hà Nội',
+	'0996457445'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10004,
+	N'Nhật Minh Quang',
+	N'Phường Dương Nội, Hà Đông, Hà Nội',
+	'0944136988'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10005,
+	N'Triệu Hồng Quân',
+	N'Phường Cổ Nhuế, Cầu Giấy, Hà Nội',
+	'0834417969'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10006,
+	N'Nghiêm Gia Bảo',
+	N'Phường Dương Nội, Hà Đông, Hà Nội',
+	'0998913462'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10007,
+	N'Hà Văn Võ Quyền',
+	N'Phường La Nội, Hà Đông, Hà Nội',
+	'0894417969'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10008,
+	N'Khuất Thuỳ Linh',
+	N'Phường Thanh Xuân, Thanh Xuân, Hà Nội',
+	'0843746194'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10009,
+	N'Vũ Ngọc Linh',
+	N'Phường Hoàn Kiếm , Hoàn Kiếm, Hà Nội',
+	'0849294649'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10010,
+	N'Nguyễn Quốc Bình',
+	N'Phường Tây Mỗ , Nam Từ Liêm, Hà Nội',
+	'0983182951'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10011,
+	N'Nguyễn Nông Quốc Hùng',
+	N'Phường Mỗ Lao, Hà Đông, Hà Nội',
+	'0989111567'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10012,
+	N'Vũ Thị Phương Anh',
+	N'Phường Định Công, Hoàng Mai, Hà Nội',
+	'0972673746'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10013,
+	N'Nguyễn Hà Nam Khánh',
+	N'Phường Hai Bà Trưng, Hai Bà Trưng, Hà Nội',
+	'0944365567'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10014,
+	N'Nguyễn Hà Khoa',
+	N'Phường Lạc Long Quân, Tây Hồ, Hà Nội',
+	'0966142199'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10015,
+	N'Chu Mạnh Quân',
+	N'Phường Đê La Thành, Đống Đa, Hà Nội',
+	'0878625816'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10016,
+	N'Nguyễn Tiến Dũng',
+	N'Phường Mỹ Đình, Nam Từ Liêm, Hà Nội',
+	'0886856120'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10017,
+	N'Nguyễn Hoàng Phúc',
+	N'Phường Mỗ Lao, Hà Đông, Hà Nội',
+	'0995455996'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10018,
+	N'Ngô Trần Bình Dương',
+	N'Phường Khương Mai, Thanh Xuân, Hà Nội',
+	'0991552829'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10019,
+	N'Trần Quang Khải',
+	N'Phường Vạn Phúc, Hà Đông, Hà Nội',
+	'0834627576'
+);
+GO
+
+INSERT INTO Readers (reader_id, reader_name, address, contact_number)
+VALUES (
+	10020,
+	N'Nguyễn Lan Hương',
+	N'Phường Phú Thượng, Tây Hồ, Hà Nội',
+	'0967447116'
+);
+GO
