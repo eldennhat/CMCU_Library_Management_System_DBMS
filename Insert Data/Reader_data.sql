2ae36a46e373c3f489a008e1a6c528605fb956a4
Hwng_012 <135439593+hoaquyen666@users.noreply.github.com>
2025-10-23 22:25:51 +0700

    Delete redundant attributes

--- Insert Data/Reader_data.sql
+++ Insert Data/Reader_data.sql
@@ -2,7 +2,6 @@
 VALUES (
 	10001,
 	N'Đinh Nguyên Hoàng',
-	N'Phường Quan Hoa, Cầu Giấy, Hà Nội',
 	'0873448820'
 );
 GO
@@ -11,7 +10,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10002,
 	N'Phạm Tuấn Hưng',
-	N'Phường Cổ Loa, Long Biên, Hà Nội',
 	'0987778822'
 );
 GO
@@ -20,7 +18,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10003,
 	N'Nguyễn Thành An',
-	N'Phường Mai Dịch, Cầu Giấy, Hà Nội',
 	'0996457445'
 );
 GO
@@ -29,7 +26,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10004,
 	N'Nhật Minh Quang',
-	N'Phường Dương Nội, Hà Đông, Hà Nội',
 	'0944136988'
 );
 GO
@@ -38,7 +34,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10005,
 	N'Triệu Hồng Quân',
-	N'Phường Cổ Nhuế, Cầu Giấy, Hà Nội',
 	'0834417969'
 );
 GO
@@ -47,7 +42,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10006,
 	N'Nghiêm Gia Bảo',
-	N'Phường Dương Nội, Hà Đông, Hà Nội',
 	'0998913462'
 );
 GO
@@ -56,7 +50,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10007,
 	N'Hà Văn Võ Quyền',
-	N'Phường La Nội, Hà Đông, Hà Nội',
 	'0894417969'
 );
 GO
@@ -65,7 +58,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10008,
 	N'Khuất Thuỳ Linh',
-	N'Phường Thanh Xuân, Thanh Xuân, Hà Nội',
 	'0843746194'
 );
 GO
@@ -74,7 +66,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10009,
 	N'Vũ Ngọc Linh',
-	N'Phường Hoàn Kiếm , Hoàn Kiếm, Hà Nội',
 	'0849294649'
 );
 GO
@@ -83,7 +74,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10010,
 	N'Nguyễn Quốc Bình',
-	N'Phường Tây Mỗ , Nam Từ Liêm, Hà Nội',
 	'0983182951'
 );
 GO
@@ -92,7 +82,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10011,
 	N'Nguyễn Nông Quốc Hùng',
-	N'Phường Mỗ Lao, Hà Đông, Hà Nội',
 	'0989111567'
 );
 GO
@@ -101,7 +90,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10012,
 	N'Vũ Thị Phương Anh',
-	N'Phường Định Công, Hoàng Mai, Hà Nội',
 	'0972673746'
 );
 GO
@@ -110,7 +98,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10013,
 	N'Nguyễn Hà Nam Khánh',
-	N'Phường Hai Bà Trưng, Hai Bà Trưng, Hà Nội',
 	'0944365567'
 );
 GO
@@ -119,7 +106,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10014,
 	N'Nguyễn Hà Khoa',
-	N'Phường Lạc Long Quân, Tây Hồ, Hà Nội',
 	'0966142199'
 );
 GO
@@ -128,7 +114,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10015,
 	N'Chu Mạnh Quân',
-	N'Phường Đê La Thành, Đống Đa, Hà Nội',
 	'0878625816'
 );
 GO
@@ -137,7 +122,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10016,
 	N'Nguyễn Tiến Dũng',
-	N'Phường Mỹ Đình, Nam Từ Liêm, Hà Nội',
 	'0886856120'
 );
 GO
@@ -146,7 +130,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10017,
 	N'Nguyễn Hoàng Phúc',
-	N'Phường Mỗ Lao, Hà Đông, Hà Nội',
 	'0995455996'
 );
 GO
@@ -155,7 +138,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10018,
 	N'Ngô Trần Bình Dương',
-	N'Phường Khương Mai, Thanh Xuân, Hà Nội',
 	'0991552829'
 );
 GO
@@ -164,7 +146,6 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10019,
 	N'Trần Quang Khải',
-	N'Phường Vạn Phúc, Hà Đông, Hà Nội',
 	'0834627576'
 );
 GO
@@ -173,7 +154,7 @@ INSERT INTO Readers (reader_id, reader_name, address, contact_number)
 VALUES (
 	10020,
 	N'Nguyễn Lan Hương',
-	N'Phường Phú Thượng, Tây Hồ, Hà Nội',
 	'0967447116'
 );
+
 GO
