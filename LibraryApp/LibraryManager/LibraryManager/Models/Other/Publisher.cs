b623aea3dea2367a677d7c2721db137f18a51d34
Hwng_012 <135439593+hoaquyen666@users.noreply.github.com>
2025-11-02 16:10:35 +0700

    Rebuild

---
+++ LibraryApp/LibraryManager/LibraryManager/Models/Other/Publisher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager.Models.Other
+{
+    public class Publisher
+    {
+        public string IdPublisher { get; set; } = string.Empty;
+        public string NamePublisher { get; set; } = string.Empty;
+
+        public Publisher(string idPublisher, string namePublisher)
+        {
+            IdPublisher = idPublisher;
+            NamePublisher = namePublisher;
+        }
+    }
+}
