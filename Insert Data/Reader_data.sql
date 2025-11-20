e887863b844940b7fa8d4fa6567e87719a29fdb9
Thành An <thanhan1552006@gmail.com>
2025-11-20 13:51:54 +0700

    50 Data for Reader

--- Insert Data/Reader_data.sql
+++ Insert Data/Reader_data.sql
@@ -1,162 +1,35 @@
-﻿INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10001,
-	N'Nguyễn Văn An',
-	'0873448820'
-);
+USE LibraryDB
 GO
 
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10002,
-	N'Phạm Thị Nhàn',
-	'0987778822'
-);
+-- 1. Thêm 50 Độc giả
+INSERT INTO Reader (FullName, Phone, [Address]) VALUES
+(N'Nguyễn Văn A', '0901000001', N'Hà Nội'), (N'Trần Thị B', '0901000002', N'Hà Nam'),
+(N'Lê Văn C', '0901000003', N'Nam Định'), (N'Phạm Thị D', '0901000004', N'Thái Bình'),
+(N'Hoàng Văn E', '0901000005', N'Ninh Bình'), (N'Vũ Thị F', '0901000006', N'Thanh Hóa'),
+(N'Đặng Văn G', '0901000007', N'Nghệ An'), (N'Bùi Thị H', '0901000008', N'Hà Tĩnh'),
+(N'Đỗ Văn I', '0901000009', N'Quảng Bình'), (N'Hồ Thị K', '0901000010', N'Quảng Trị'),
+(N'Ngô Văn L', '0901000011', N'Huế'), (N'Dương Thị M', '0901000012', N'Đà Nẵng'),
+(N'Lý Văn N', '0901000013', N'Quảng Nam'), (N'Vương Thị O', '0901000014', N'Quảng Ngãi'),
+(N'Phan Văn P', '0901000015', N'Bình Định'), (N'Trương Thị Q', '0901000016', N'Phú Yên'),
+(N'Nguyễn Văn R', '0901000017', N'Khánh Hòa'), (N'Trần Thị S', '0901000018', N'Ninh Thuận'),
+(N'Lê Văn T', '0901000019', N'Bình Thuận'), (N'Phạm Thị U', '0901000020', N'Đồng Nai'),
+(N'Hoàng Văn V', '0901000021', N'Bình Dương'), (N'Vũ Thị X', '0901000022', N'Vũng Tàu'),
+(N'Đặng Văn Y', '0901000023', N'TP HCM'), (N'Bùi Thị Z', '0901000024', N'Long An'),
+(N'Đỗ Văn AA', '0901000025', N'Tiền Giang'), (N'Hồ Thị BB', '0901000026', N'Bến Tre'),
+(N'Ngô Văn CC', '0901000027', N'Trà Vinh'), (N'Dương Thị DD', '0901000028', N'Vĩnh Long'),
+(N'Lý Văn EE', '0901000029', N'Cần Thơ'), (N'Vương Thị FF', '0901000030', N'Hậu Giang'),
+(N'Phan Văn GG', '0901000031', N'Sóc Trăng'), (N'Trương Thị HH', '0901000032', N'Bạc Liêu'),
+(N'Nguyễn Văn II', '0901000033', N'Cà Mau'), (N'Trần Thị KK', '0901000034', N'Kiên Giang'),
+(N'Lê Văn LL', '0901000035', N'An Giang'), (N'Phạm Thị MM', '0901000036', N'Đồng Tháp'),
+(N'Hoàng Văn NN', '0901000037', N'Tây Ninh'), (N'Vũ Thị OO', '0901000038', N'Bình Phước'),
+(N'Đặng Văn PP', '0901000039', N'Đắk Nông'), (N'Bùi Thị QQ', '0901000040', N'Đắk Lắk'),
+(N'Đỗ Văn RR', '0901000041', N'Gia Lai'), (N'Hồ Thị SS', '0901000042', N'Kon Tum'),
+(N'Ngô Văn TT', '0901000043', N'Lâm Đồng'), (N'Dương Thị UU', '0901000044', N'Lào Cai'),
+(N'Lý Văn VV', '0901000045', N'Yên Bái'), (N'Vương Thị XX', '0901000046', N'Sơn La'),
+(N'Phan Văn YY', '0901000047', N'Hòa Bình'), (N'Trương Thị ZZ', '0901000048', N'Hải Phòng'),
+(N'Đinh Văn AA1', '0901000049', N'Hải Dương'), (N'Lâm Thị BB1', '0901000050', N'Hưng Yên');
+GO
 GO
 
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10003,
-	N'Nguyễn Thành Công',
-	'0996457445'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10004,
-	N'Nguyễn Nhật Minh',
-	'0944136988'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10005,
-	N'Triệu Hồng Quân',
-	'0834417969'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10006,
-	N'Nghiêm Gia Bảo',
-	'0998913462'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10007,
-	N'Hà Văn Võ Quyền',
-	'0894417969'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10008,
-	N'Khuất Thuỳ Linh',
-	'0843746194'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10009,
-	N'Vũ Ngọc Linh',
-	'0849294649'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10010,
-	N'Nguyễn Quốc Bình',
-	'0983182951'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10011,
-	N'Nguyễn Nông Quốc Hùng',
-	'0989111567'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10012,
-	N'Vũ Thị Phương Anh',
-	'0972673746'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10013,
-	N'Nguyễn Hà Nam Khánh',
-	'0944365567'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10014,
-	N'Nguyễn Hà Khoa',
-	'0966142199'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10015,
-	N'Chu Mạnh Quân',
-	'0878625816'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10016,
-	N'Nguyễn Tiến Dũng',
-	'0886856120'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10017,
-	N'Nguyễn Hoàng Phúc',
-	'0995455996'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10018,
-	N'Ngô Trần Bình Dương',
-	'0991552829'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10019,
-	N'Trần Quang Khải',
-	'0834627576'
-);
-GO
-
-INSERT INTO Readers (reader_id, reader_name, contact_number)
-VALUES (
-	10020,
-	N'Nguyễn Lan Hương',
-	'0967447116'
-);
-
-GO
 
 
