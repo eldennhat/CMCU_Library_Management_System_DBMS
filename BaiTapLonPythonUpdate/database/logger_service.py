fe2fe65047fd259c151eedfad534060478a56d89
Nhat Minh Quang <eldennhat@gmail.com>
2025-12-02 14:22:36 +0700

    add System_logs (MongoDB)

---
+++ BaiTapLonPythonUpdate/database/logger_service.py
@@ -0,0 +1,33 @@
+import pymongo
+from datetime import datetime
+
+
+
+class SystemLog:
+    def __init__(self):
+        # connect to MongoDB
+        # default localhost:27017
+        try:
+            self.client = pymongo.MongoClient("mongodb://localhost:27017")
+            #choose a database and collection
+            self.db = self.client["LibraryDB"]
+            self.collection = self.db["system_log"]
+            print("MongoDB Connected")
+        except Exception as e:
+            print(f"Error connecting to MongoDB: {e}")
+    def insert_log(self, username, action, message, detail = None ):
+        #insert log to mongoDB
+        log_data = {
+            "username": username,
+            "action": action,
+            "message": message,
+            "detail": detail if detail else {},
+            "system" : "Library App", #danh dau nguon log
+            "time": datetime.now()
+        }
+        try:
+            self.collection.insert_one(log_data)
+            print(f"Logged: {action} - {status}")
+        except Exception as e:
+            print(f"Error: {e}")
+logger = SystemLog()
